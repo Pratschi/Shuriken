1ca969c7bd4a6be29ac81be91bba61eb81dc9a64
Pratschi <pratschigamer@gmail.com>
2025-12-10 09:36:31 +0100

    [v0.0.1] Create boot.c for UEFI

---
+++ Releases/Shuriken_0.0.1/sys/boot/uefi/boot.c
@@ -0,0 +1,58 @@
+#include <efi.h>
+#include <efilib.h>
+
+// BootInfo shared with kernel
+struct BootInfo {
+    UINT64 fb_addr;
+    UINT32 fb_width;
+    UINT32 fb_height;
+    UINT32 fb_pitch;
+    UINT8 boot_type; // 1 = UEFI
+};
+
+EFI_STATUS efi_main(EFI_HANDLE img, EFI_SYSTEM_TABLE *sys)
+{
+    InitializeLib(img, sys);
+    Print(L"Shuriken UEFI Bootloader...\n");
+
+    EFI_FILE_IO_INTERFACE *io;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fsp;
+    EFI_FILE *root, *file;
+
+    sys->BootServices->HandleProtocol(
+        img, &gEfiSimpleFileSystemProtocolGuid,
+        (void**)&fsp);
+
+    fsp->OpenVolume(fsp, &root);
+
+    if (root->Open(root, &file, L"kernel.bin",
+                   EFI_FILE_MODE_READ, 0) != EFI_SUCCESS) {
+        Print(L"Error: kernel.bin not found\n");
+        return EFI_LOAD_ERROR;
+    }
+
+    UINTN size = 1024*1024;
+    UINT8 *kernel = AllocatePool(size);
+    file->Read(file, &size, kernel);
+
+    // Get framebuffer
+    EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
+    sys->BootServices->LocateProtocol(
+        &gEfiGraphicsOutputProtocolGuid, NULL,
+        (void**)&gop);
+
+    struct BootInfo *b = AllocatePool(sizeof(struct BootInfo));
+    b->fb_addr = gop->Mode->FrameBufferBase;
+    b->fb_width = gop->Mode->Info->HorizontalResolution;
+    b->fb_height = gop->Mode->Info->VerticalResolution;
+    b->fb_pitch = gop->Mode->Info->PixelsPerScanLine * 4;
+    b->boot_type = 1;
+
+    // Kernel entry
+    void (*kentry)(struct BootInfo*) =
+        ((__attribute__((ms_abi)) void(*)(struct BootInfo*))kernel);
+
+    kentry(b);
+
+    return EFI_SUCCESS;
+}
